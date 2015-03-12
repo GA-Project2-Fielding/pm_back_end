8ec71d91180c2a7cbcbe131167ac0783b730c3a1
abroccoli <acbrocco@syr.edu>
2015-03-12 10:48:37 -0400

    added task dependency to files

--- db/migrate/20150312144224_create_files.rb
+++ db/migrate/20150312144224_create_files.rb
@@ -3,8 +3,10 @@ class CreateFiles < ActiveRecord::Migration
     create_table :files do |t|
       t.string :name
       t.string :url, null: false
+      t.belongs_to :task, :index: true
 
       t.timestamps
     end
+    add_foreign_key :files, :tasks
   end
 end
