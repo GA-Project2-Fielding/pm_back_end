512079e90f97593ede8bae1fe29f4dd4cb6a4c03
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-03-12 12:47:33 -0400

    Add Project model and validations

---
+++ app/models/project.rb
@@ -0,0 +1,15 @@
+class Project < ActiveRecord::Base
+  after_initialize :defaults
+
+  validates :project_title, presence: true
+  validates :start_date, presence: true
+
+  has_many :collaborators
+  has_many :users, through: :collaborators
+
+  has_many :tasks, dependent: :destroy
+
+  def defaults
+   self.completed = false if self.completed.nil?
+  end
+end
