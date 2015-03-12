5f2e5c6609cf4889e40e803a248e37a475e25e38
Dan Stoppelman <dstop75@gmail.com>
2015-03-12 16:16:57 -0400

    include serialization and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include ActionController::Serialization
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      User.find_by(token: token)
+    end
+  end
 end
