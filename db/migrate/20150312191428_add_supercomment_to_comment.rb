637fd90e18e00e54e992acbd621f0d5b8f76b09a
abroccoli <acbrocco@syr.edu>
2015-03-12 15:35:22 -0400

    removed migration files for adding self reference columns

--- db/migrate/20150312191428_add_supercomment_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddSupercommentToComment < ActiveRecord::Migration
-  def change
-    add_column :comments, :references, :supercomment
-  end
-end
--- db/migrate/20150312191445_add_supertask_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddSupertaskToTask < ActiveRecord::Migration
-  def change
-    add_column :tasks, :references, :supertask
-  end
-end
