48fb599b8d7795cad5f4e01e4e153c769ed84e31
abroccoli <acbrocco@syr.edu>
2015-03-12 15:13:44 -0400

    removed wrong migrations

--- db/migrate/20150312191015_add_supertask_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddSupertaskToTask < ActiveRecord::Migration
-  def change
-    add_column :tasks, :belongs_to, :supertask
-  end
-end
--- db/migrate/20150312191111_add_supercomment_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddSupercommentToComment < ActiveRecord::Migration
-  def change
-    add_column :comments, :belongs_to, :supercomment
-  end
-end
