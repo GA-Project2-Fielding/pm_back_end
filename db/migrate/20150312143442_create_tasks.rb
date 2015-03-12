5eb56204a6b60753adc4a21207724374a39e63bb
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-03-12 10:43:39 -0400

    Add tasks migration

---
+++ db/migrate/20150312143442_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.datetime :due_date
+      t.boolean :completed, null: false, default: false
+      t.integer :priority, null: false, default: 0
+      t.string :title, null: false
+      t.text :description
+      t.belongs_to :project, index: true
+
+      t.timestamps
+    end
+    add_foreign_key :tasks, :projects
+  end
+end
