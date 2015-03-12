516869eccef8ee1c6a2bd6c425ef9cc505799bca
Dan Stoppelman <dstop75@gmail.com>
2015-03-12 11:00:14 -0400

    create Comments table

---
+++ db/migrate/20150312145517_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.belongs_to :task, index: true
+      t.belongs_to :user, index: true
+    end
+    add_foreign_key :comments, :tasks
+    add_foreign_key :comments, :users
+  end
+end
