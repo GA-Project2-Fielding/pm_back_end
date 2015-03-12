5c742ba7cf119b07a3ab3f66b11387f33348474b
Dan Stoppelman <dstop75@gmail.com>
2015-03-12 10:13:30 -0400

    create Users table migration

---
+++ db/migrate/20150312140253_create_user.rb
@@ -0,0 +1,15 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :user_name, null: false
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :password_digest, null: false
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
