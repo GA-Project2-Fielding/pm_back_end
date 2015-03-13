9519a52a7773898fe73edab5f39a2705ec0e51ed
Dan Stoppelman <dstop75@gmail.com>
2015-03-12 20:56:13 -0400

    remove root node from json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
+  def default_serializer_options
+    {root: false}
+  end
+
   protected
 
-  def authenticate
-    authenticate_or_request_with_http_token do |token, options|
-      User.find_by(token: token)
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        User.find_by(token: token)
+      end
     end
-  end
 end
