c871c052df31056fabe3aa386f862c3d55a413d7
Dan Stoppelman <dstop75@gmail.com>
2015-03-12 22:29:01 -0400

    add ids to serializers outputs

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -4,7 +4,7 @@ class TaskSerializer < ActiveModel::Serializer
   has_many :users, through: :user_tasks
   has_many :subtasks
 
-  attributes :due_date, :completed, :priority, :title, :description, :created_at
+  attributes :id, :due_date, :completed, :priority, :title, :description, :created_at
 
   def created_at
     object.created_at.strftime('%m-%d-%Y')
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,5 +5,5 @@ class UserSerializer < ActiveModel::Serializer
   has_many :tasks, through: :user_tasks
   has_many :comments
 
-  attributes :user_name, :email, :first_name, :last_name, :image_url, :description
+  attributes :id, :user_name, :email, :first_name, :last_name, :image_url, :description
 end
