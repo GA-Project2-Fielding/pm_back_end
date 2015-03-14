1ce89a3b1dbdcf252003f7b244f99e73b87251ed
abroccoli <acbrocco@syr.edu>
2015-03-14 12:06:32 -0400

    wrote create subcomment method in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,5 +11,8 @@ class Comment < ActiveRecord::Base
 
   def new_subcomment(comment_params)
     body = comment_params['body']
+
+    task_id = self.task_id
+    self.subcomment.new(body: body, task_id: task_id)
   end
 end
