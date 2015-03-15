d339b614395d6d78722ea0db416fb37c1016ee51
Dan Stoppelman <dstop75@gmail.com>
2015-03-14 21:45:22 -0400

    rename files to file_locations to avoid keyword

--- db/migrate/20150312144224_create_file_locations.rb
+++ db/migrate/20150312144224_create_file_locations.rb
@@ -1,12 +1,12 @@
-class CreateFiles < ActiveRecord::Migration
+class CreateFileLocations < ActiveRecord::Migration
   def change
-    create_table :files do |t|
+    create_table :file_locations do |t|
       t.string :name
       t.string :url, null: false
       t.belongs_to :task, index: true
 
       t.timestamps
     end
-    add_foreign_key :files, :tasks
+    add_foreign_key :file_locations, :tasks
   end
 end
